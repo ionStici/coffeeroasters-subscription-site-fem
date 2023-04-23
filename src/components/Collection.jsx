9d6a005cc24089691bd1e16a1e04eb9f75b6ade1
Ion Stici <ionsticidev@gmail.com>
2023-04-23 08:33:31 +0300

    Add Collection Component

---
+++ src/components/Collection.jsx
@@ -0,0 +1,67 @@
+import styles from './../styles/Collection.module.scss';
+import danche from './../assets/home/desktop/image-danche.png';
+import piccollo from './../assets/home/desktop/image-piccollo.png';
+import planalto from './../assets/home/desktop/image-planalto.png';
+import gran from './../assets/home/desktop/image-gran-espresso.png';
+
+const Collection = function () {
+    return (
+        <section className={styles.section}>
+            <div className={styles.wrapper}>
+                <div className={styles.header}>
+                    <h2 className={styles.h2}>Our Collection</h2>
+                </div>
+
+                <div className={styles.card}>
+                    <img
+                        className={styles.img}
+                        src={gran}
+                        alt="Gran Espresso"
+                    />
+                    <div className={styles.box}>
+                        <h3 className={styles.h3}>Gran Espresso</h3>
+                        <p className={styles.text}>
+                            Light and flavorful blend with cocoa and black
+                            pepper for an intense experience
+                        </p>
+                    </div>
+                </div>
+
+                <div className={styles.card}>
+                    <img className={styles.img} src={planalto} alt="Planalto" />
+                    <div className={styles.box}>
+                        <h3 className={styles.h3}>Planalto</h3>
+                        <p className={styles.text}>
+                            Brazilian dark roast with rich and velvety body, and
+                            hints of fruits and nuts
+                        </p>
+                    </div>
+                </div>
+
+                <div className={styles.card}>
+                    <img className={styles.img} src={piccollo} alt="Piccollo" />
+                    <div className={styles.box}>
+                        <h3 className={styles.h3}>Piccollo</h3>
+                        <p className={styles.text}>
+                            Mild and smooth blend featuring notes of toasted
+                            almond and dried cherry
+                        </p>
+                    </div>
+                </div>
+
+                <div className={styles.card}>
+                    <img className={styles.img} src={danche} alt="Danche" />
+                    <div className={styles.box}>
+                        <h3 className={styles.h3}>Danche</h3>
+                        <p className={styles.text}>
+                            Ethiopian hand-harvested blend densely packed with
+                            vibrant fruit notes
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Collection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,20 @@
 import Header from './../components/Header';
 import Hero from './../components/Hero';
+import Collection from '../components/Collection';
 
 const Home = function () {
     return (
         <>
             <Header />
-            <Hero
-                page="home"
-                title="Great coffee made simple."
-                text="Start your mornings with the world’s best coffees. Try our expertly curated artisan coffees from our best roasters delivered directly to your door, at your schedule."
-                button="Create your plan"
-            />
+            <main>
+                <Hero
+                    page="home"
+                    title="Great coffee made simple."
+                    text="Start your mornings with the world’s best coffees. Try our expertly curated artisan coffees from our best roasters delivered directly to your door, at your schedule."
+                    button="Create your plan"
+                />
+                <Collection />
+            </main>
         </>
     );
 };
