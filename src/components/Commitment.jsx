036559379d330043458e5e26cfaf6bdecbd2a182
Ion Stici <ionsticidev@gmail.com>
2023-04-30 17:05:36 +0300

    Finish Commitment Component

--- src/components/Commitment.jsx
+++ src/components/Commitment.jsx
@@ -15,27 +15,26 @@ const Commitment = function () {
                         src={baristaDesktop}
                         alt="Barista brewing coffee"
                     />
-                    <div className={styles.content_wrapper}>
-                        <h1 className={styles.title}>Our commitment</h1>
-                        <p className={styles.text}>
-                            We’re built on a simple mission and a commitment to
-                            doing good along the way. We want to make it easy
-                            for you to discover and brew the world’s best coffee
-                            at home. It all starts at the source. To locate the
-                            specific lots we want to purchase, we travel nearly
-                            60 days a year trying to understand the challenges
-                            and opportunities in each of these places. We
-                            collaborate with exceptional coffee growers and
-                            empower a global community of farmers through with
-                            well above fair-trade benchmarks. We also offer
-                            training, support farm community initiatives, and
-                            invest in coffee plant science. Curating only the
-                            finest blends, we roast each lot to highlight
-                            tasting profiles distinctive to their native growing
-                            region.
-                        </p>
-                    </div>
                 </picture>
+                <div className={styles.content_wrapper}>
+                    <h1 className={styles.title}>Our commitment</h1>
+                    <p className={styles.text}>
+                        We’re built on a simple mission and a commitment to
+                        doing good along the way. We want to make it easy for
+                        you to discover and brew the world’s best coffee at
+                        home. It all starts at the source. To locate the
+                        specific lots we want to purchase, we travel nearly 60
+                        days a year trying to understand the challenges and
+                        opportunities in each of these places. We collaborate
+                        with exceptional coffee growers and empower a global
+                        community of farmers through with well above fair-trade
+                        benchmarks. We also offer training, support farm
+                        community initiatives, and invest in coffee plant
+                        science. Curating only the finest blends, we roast each
+                        lot to highlight tasting profiles distinctive to their
+                        native growing region.
+                    </p>
+                </div>
             </div>
         </section>
     );
