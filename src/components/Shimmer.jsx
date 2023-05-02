7c83a09a610eb1bd984072f4fe42f22f51f900ee
Ion Stici <ionsticidev@gmail.com>
2023-05-02 13:14:39 +0300

    Update Shimmer

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -4,36 +4,59 @@ import PropTypes from 'prop-types';
 
 const Shimmer = function (props) {
     const layout = React.useRef(null);
+    const cover = React.useRef(null);
     props.toTop();
 
     React.useEffect(() => {
+        const body = document.body;
+        body.classList.add(styles.overflow_hidden);
+
         if (props.removeShimmer) {
             layout.current.classList.add(styles.hide);
+            cover.current.classList.add(styles.hide);
+            body.classList.remove(styles.overflow_hidden);
             props.toTop();
         }
     }, [props.removeShimmer]);
 
     return (
-        <div className={styles.shimmer} ref={layout}>
-            <div className={styles.wrapper}>
-                <div className={styles.nav}></div>
-                {/* prettier-ignore */}
-                <div className={styles.hero} style={{ height: `${props.height}px` }}></div>
-                <div className={styles.title}></div>
+        <>
+            <div className={styles.cover} ref={cover}></div>
+            <div className={styles.shimmer} ref={layout}>
+                <div className={styles.wrapper}>
+                    <div className={styles.nav}></div>
+                    {/* prettier-ignore */}
+                    <div className={styles.hero} style={{ height: `${props.height}px` }}></div>
+                    <div className={styles.collection_title}></div>
 
-                <div className={styles.img}></div>
-                <div className={styles.name}></div>
-                <div className={styles.text}></div>
+                    <div className={styles.boxes_wrapper}>
+                        <div className={styles.content_wrapper}>
+                            <div className={styles.img}></div>
+                            <div className={styles.name}></div>
+                            <div className={styles.text}></div>
+                        </div>
 
-                <div className={styles.img}></div>
-                <div className={styles.name}></div>
-                <div className={styles.text}></div>
+                        <div className={styles.content_wrapper}>
+                            <div className={styles.img}></div>
+                            <div className={styles.name}></div>
+                            <div className={styles.text}></div>
+                        </div>
 
-                <div className={styles.img}></div>
-                <div className={styles.name}></div>
-                <div className={styles.text}></div>
+                        <div className={styles.content_wrapper}>
+                            <div className={styles.img}></div>
+                            <div className={styles.name}></div>
+                            <div className={styles.text}></div>
+                        </div>
+
+                        <div className={styles.content_wrapper}>
+                            <div className={styles.img}></div>
+                            <div className={styles.name}></div>
+                            <div className={styles.text}></div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
