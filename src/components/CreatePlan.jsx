61ef050fa3f266877685eaa53d83f58604953266
Ion Stici <ionsticidev@gmail.com>
2023-05-04 09:04:34 +0300

    Update Create Plan Component

--- src/components/CreatePlan.jsx
+++ src/components/CreatePlan.jsx
@@ -5,32 +5,48 @@ import PropTypes from 'prop-types';
 
 const CreatePlan = function (props) {
     const data = props.data;
-    // prettier-ignore
-    const iconArrow = (<svg width="19" height="13" xmlns="http://www.w3.org/2000/svg"><path d="M15.949.586l2.828 2.828-9.096 9.096L.586 3.414 3.414.586l6.267 6.267z" fill="#0E8784" fillRule="nonzero"/></svg>);
 
     const handleDropdown = function ({ target }) {
         const block = target.closest(`.${styles.accordion__block}`);
         const arrow = block.querySelector('svg');
-        let status = block.dataset.dropdown;
         const boxes = block.querySelector(`.${styles.accordion__boxes}`);
 
-        if (status === 'close') {
+        if (block.dataset.dropdown === 'close') {
             arrow.classList.add(styles.accordion__rotate);
-            boxes.classList.add(styles.display);
-            setTimeout(() => boxes.classList.add(styles.render), 1);
+            boxes.classList.add(styles.display_boxes);
+            setTimeout(() => boxes.classList.add(styles.render_boxes), 1);
 
             block.dataset.dropdown = 'open';
             return;
         }
 
-        if (status === 'open') {
+        if (block.dataset.dropdown === 'open') {
             arrow.classList.remove(styles.accordion__rotate);
-            boxes.classList.remove(styles.render);
-            setTimeout(() => boxes.classList.remove(styles.display), 250);
+            boxes.classList.remove(styles.render_boxes);
+            setTimeout(() => boxes.classList.remove(styles.display_boxes), 250);
 
             block.dataset.dropdown = 'close';
             return;
         }
+    };
+
+    const setBoxActive = function ({ target }) {
+        if (target.classList.contains(styles.accordion__box__active)) {
+            target.classList.remove(styles.accordion__box__active);
+            return;
+        }
+
+        const boxes = [
+            ...target
+                .closest(`.${styles.accordion__boxes}`)
+                .querySelectorAll(`.${styles.accordion__box}`),
+        ];
+
+        boxes.forEach(box => {
+            box.classList.remove(styles.accordion__box__active);
+        });
+
+        target.classList.add(styles.accordion__box__active);
     };
 
     return (
@@ -42,22 +58,20 @@ const CreatePlan = function (props) {
                 <div className={styles.accordion}>
                     {data.map((q, i) => {
                         return (
-                            <div
-                                className={styles.accordion__block}
-                                key={i}
-                                data-dropdown={'close'}
-                            >
+                            // prettier-ignore
+                            <div className={`${styles.accordion__block}`} key={i} data-dropdown={q.id === 1 ? 'open' : 'close'}>
                                 {/* prettier-ignore */}
                                 <button className={`${styles.accordion__btn}`} onClick={handleDropdown}>
                                     <h2>{q.question}</h2>
-                                    <span>{iconArrow}</span>
+                                    {/* prettier-ignore */}
+                                    <span>{<svg className={q.id === 1 ? styles.accordion__rotate : ''} width="19" height="13" xmlns="http://www.w3.org/2000/svg"><path d="M15.949.586l2.828 2.828-9.096 9.096L.586 3.414 3.414.586l6.267 6.267z" fill="#0E8784" fillRule="nonzero"/></svg>}</span>
                                 </button>
-
-                                <div className={styles.accordion__boxes}>
+                                {/* prettier-ignore */}
+                                <div className={`${styles.accordion__boxes} ${q.id === 1 ? styles.display_boxes : ''} ${q.id === 1 ? styles.render_boxes : ''}`}>
                                     {q.answers.map((a, i) => {
                                         return (
                                             // prettier-ignore
-                                            <div className={`${styles.accordion__box}`} key={i}>
+                                            <div className={`${styles.accordion__box}`} key={i} onClick={setBoxActive}>
                                                 <h3>{a.title}</h3>
                                                 <p>{a.text}</p>
                                             </div>
